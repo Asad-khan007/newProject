1ee493f54062ab4e452c2bb16480f33aec692724
Asad-khan007 <69400466+Asad-khan007@users.noreply.github.com>
2020-08-16 06:46:12 -0700

    update the javascript file
    re write the code of javascript in new modern way

--- project01/script.js
+++ project01/script.js
@@ -1,59 +1,56 @@
 const form = document.getElementById('form');
 const username = document.getElementById('username');
-const email = document.getElementById('Email');
-const password = document.getElementById('Password');
-const password2 = document.getElementById('Password2');
-
-
-// All fuctions 
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
 
 
-// Fuction to show the error
 
-function showError (input , message) {
-  const formControl = input.parentElement;
-  formControl.className='form-control error';
-  const small = formControl.querySelector('small');
-  small.innerText =  message
-}
 
-// Fuction to show the Success
+//All functions 
 
-function showSuccess(input ) {
+//Function to show success 
+
+function showSuccess(input) {
     const formControl = input.parentElement;
-    formControl.className='form-control success';
+    formControl.className = 'form-control success';
 }
 
-// Fuction to check Email is valid
+//Function for show Error 
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message
+}
+// Function to check if email is valid
 
-function emailValid(email) {
+function validationEmail(email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    return re.test(String(email).toLowerCase()); 
 }
 
-// Fuction to check if require feilds have data
+// Funtion to check required field have data
 
 function checkRequired(inputArray) {
-  inputArray.forEach(function(input) {
-      if(input.value === '') {
-          showError(input , `${getFieldId(input)} is required `);
-      } else {
-          showSuccess(input);
-      }
-  })
+     inputArray.forEach(function(input) {
+         if(input.value === '') {
+             showError(input ,`${getProper(input)} is required`)
+         } else {
+             showSuccess (input);
+         }
+     });
+}
+// Function to convert in proper case 
+
+function getProper(input) {
+      return input.id.charAt(0).toUpperCase() + input.id.slice(1) ;
 }
 
-// Fuction to get the id of the input field with proper case
-
-function getFieldId(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-
-// this is an event listener for the form
+// this is an event listener for the form 
 form.addEventListener('submit' , function(e) {
-     e.preventDefault();
-     
-     checkRequired([username,email,password,password2]);
- })
+    e.preventDefault();
+    
+    checkRequired([username,email,password,password2])
+})
