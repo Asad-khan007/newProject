b1468f8b5eafd5294935337d9fcd35cac487fd8a
Asad-khan007 <69400466+Asad-khan007@users.noreply.github.com>
2020-08-16 11:06:50 -0700

    Update script.js
    new modification on js file for sign up form

--- project01/script.js
+++ project01/script.js
@@ -26,9 +26,13 @@ function showError(input, message) {
 }
 // Function to check if email is valid
 
-function validationEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase()); 
+    if (re.test(input.value.trim()) ) {
+        showSuccess(input)
+    } else {
+        showError(input,`Please provide a valid Email`)
+    }
 }
 
 // Funtion to check required field have data
@@ -60,11 +64,21 @@ function checkLength(input, min , max) {
    }
 }
 
+// Function to check if password was matched
+
+function matchPas(input, input2) {
+    if(input.value !== input2.value) {
+        showError(input2, 'password is not match')
+    }
+}
+
 // this is an event listener for the form 
 form.addEventListener('submit' , function(e) {
     e.preventDefault();
     
-    checkRequired([username,email,password,password2])
-    checkLength(username,3,10)
-    checkLength(password,6,30)
+    checkRequired([username,email,password,password2]);
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
+    matchPas( password,password2);
 })
